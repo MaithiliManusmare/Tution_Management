e2084ba1e86cd86783d38ca09f8d58d873231249
Maithili Manusmare <maithilimanusmare65@gmail.com>
2026-01-16 23:36:03 +0530

    Changes in the batch registration form

--- app/src/main/java/com/example/assignment/BatchRegistrationActivity.kt
+++ app/src/main/java/com/example/assignment/BatchRegistrationActivity.kt
@@ -1,12 +1,28 @@
 package com.example.assignment
 
+import android.app.TimePickerDialog
+import android.graphics.Typeface
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.textfield.TextInputEditText
+import java.util.Calendar
 
 class BatchRegistrationActivity : AppCompatActivity() {
+    lateinit var nameEditText : EditText
+    lateinit var enrolledSubjectsEditText : EditText
+    lateinit var enrollStudentsEditText : EditText
+    lateinit var startTimeEt: TextInputEditText
+    lateinit var endTimeEt: TextInputEditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +32,91 @@ class BatchRegistrationActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        generateIds()
+        setOnClickListeners()
+    }
+
+    private fun setOnClickListeners() {
+        val enrolledSubjectList : List<String> = listOf("All Subjects")
+
+        enrolledSubjectsEditText.setOnClickListener{
+            showSelectionPopup("Select Subject",enrolledSubjectList, enrolledSubjectsEditText);
+        }
+        enrollStudentsEditText.setOnClickListener{
+            showSelectionPopup("Select Students",enrolledSubjectList, enrollStudentsEditText);
+        }
+        startTimeEt.setOnClickListener {
+            showTimePicker(startTimeEt)
+        }
+        endTimeEt.setOnClickListener {
+            showTimePicker(endTimeEt)
+        }
+
+    }
+    private fun showTimePicker(editText: TextInputEditText) {
+        val calendar = Calendar.getInstance()
+
+        val hour = calendar.get(Calendar.HOUR_OF_DAY)
+        val minute = calendar.get(Calendar.MINUTE)
+
+        TimePickerDialog(
+            this,
+            { _, selectedHour, selectedMinute ->
+                val formattedTime = String.format(
+                    "%02d:%02d %s",
+                    if (selectedHour > 12) selectedHour - 12 else selectedHour,
+                    selectedMinute,
+                    if (selectedHour >= 12) "PM" else "AM"
+                )
+                editText.setText(formattedTime)
+            },
+            hour,
+            minute,
+            false
+        ).show()
+    }
+
+    private fun generateIds() {
+        nameEditText = findViewById(R.id.nameEditText)
+        enrolledSubjectsEditText = findViewById(R.id.enrolledSubjectsEditText)
+        enrollStudentsEditText = findViewById(R.id.enrollStudentsEditText)
+        startTimeEt = findViewById(R.id.startTimeEt)
+        endTimeEt = findViewById(R.id.endTimeEt)
+    }
+
+    private fun showSelectionPopup(
+        title: String,
+        items: List<String>,
+        targetEditText: EditText,
+    ) {
+        val dialog = AlertDialog.Builder(this)
+            .create()
+
+        val inflater = layoutInflater
+        val view = inflater.inflate(R.layout.dialog_items_list, null)
+        dialog.setView(view)
+
+        val recyclerView = view.findViewById<RecyclerView>(R.id.recyclerViewBrands)
+        val dialogTitle = view.findViewById<TextView>(R.id.dialogTitle)
+        val closeButton = view.findViewById<ImageButton>(R.id.closeButton)
+
+        dialogTitle.setText(title)
+
+        closeButton.setOnClickListener {
+            dialog.dismiss()
+        }
+
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        val adapter = SelectOptionsAdapter(items,) { selectedItem ->
+            targetEditText.setText(selectedItem)
+            targetEditText.setTextColor(ContextCompat.getColor(this, R.color.black))
+            targetEditText.setTypeface(null, Typeface.BOLD)
+            targetEditText.textSize = 14f
+            dialog.dismiss()
+        }
+
+        recyclerView.adapter = adapter
+        dialog.show()
     }
 }
